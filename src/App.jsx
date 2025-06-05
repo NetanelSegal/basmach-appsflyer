d0baed1b0d72252d3246c0babc3af3ba53291b8a
Netanel <segal.netanel4@gmail.com>
2025-06-05 11:16:04 +0300

    Navbar ready

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello</h1>
+      <button>Click</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navlink.jsx
@@ -0,0 +1,7 @@
+export default function Navlink(props) {
+  return (
+    <li>
+      <a href=''>{props.text}</a>
+    </li>
+  );
+}
